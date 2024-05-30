1e53c3dc420c8e76c215f4f71abc64795581ba61
PremnathQHT <premnathquehive.com>
2024-05-30 15:26:29 +0530

    changes

--- todolist.py
+++ todolist.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template,redirect
 from dotenv import load_dotenv
 import os
 
@@ -10,7 +10,6 @@ app=Flask(__name__)
 @app.route('/')
 def home():
     return render_template("Home.html")
-
 
 if __name__=="__main__":
     app.run(port=port)
