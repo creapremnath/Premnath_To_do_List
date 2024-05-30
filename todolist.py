0080efd2469d07e2a7a9a97ea64b97a31d5e29c6
PremnathQHT <premnathquehive.com>
2024-05-30 15:01:41 +0530

    Initial setup

---
+++ todolist.py
@@ -0,0 +1,16 @@
+from flask import Flask, render_template
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+port=os.getenv("portnumber")
+
+app=Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template("Home.html")
+
+
+if __name__=="__main__":
+    app.run(port=port)
